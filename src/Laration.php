0fcbb5250b1542f0187c3b514c9a8eaa70a55550
marabesi <matheus.marabesi@gmail.com>
2016-10-28 23:29:49 -0200

    first commit

---
+++ src/Laration.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Laration;
+
+use Illuminate\Console\Command;
+
+class Laration extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'laration:list {option=all}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'List all environment variables used';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+	{
+	    $option = strtolower($this->argument('option'));
+	    $configs = config();
+
+        $configArray = $configs->all();
+
+        foreach ($configArray as $header => $items) {
+            foreach ($items as $key => $value) {
+                if (!is_array($value)) {
+                    $data[$header][] = [$key, $value];
+                }
+            }
+
+            if (array_key_exists($header, $data)) {
+                if ($header == $option || $option == 'all') {
+                    $this->table([$header, 'value'], $data[$header]);
+                }
+            }
+        }
+    }
+}
+
