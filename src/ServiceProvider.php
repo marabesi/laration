05a5405701fd640a68b85c1b7f115714cdd86bd7
Gabriel Caruso <carusogabriel34@gmail.com>
2017-10-14 02:04:33 -0300

    Add a ServiceProvider with package-discovery for easier use of Laration

---
+++ src/ServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Marabesi;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Laration::class,
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
