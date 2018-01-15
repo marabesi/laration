ddb99b6e6428728a4700e095156eac451cb52f71
marabesi <matheus.marabesi@gmail.com>
2018-01-15 20:11:43 -0200

    clean up

--- install-test.sh
+++ install-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 composer create-project --prefer-dist laravel/laravel test-project "5.5.*" -vv
-cd test-project && composer require marabesi/laration -vv
-cd test-project && php artisan laration:list app
+cd test-project &&
+    composer require marabesi/laration -vv &&
+    php artisan laration:list app
