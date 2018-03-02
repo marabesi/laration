90729b77afe7f35738db584a0b656230c12ff0bc
Matheus Marabesi <matheus.marabesi@gmail.com>
2018-03-02 15:52:21 -0300

    Update install.sh
    project using laravel 5.4 added

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+composer create-project --prefer-dist laravel/laravel test-project-5-4 "5.4.*" -vv
+cd test-project-5-4 &&
+    composer require marabesi/laration:1.0 -vv &&
+    php artisan laration:list app &&
+    cd .. &&
+    rm -rf test-project-5-4 &&
+
 composer create-project --prefer-dist laravel/laravel test-project-5-5 "5.5.*" -vv
 cd test-project-5-5 &&
     composer require marabesi/laration:1.1 -vv &&
