f45cf678a356585d46a014f1c63e2349bacf8db4
marabesi <matheus.marabesi@gmail.com>
2018-03-02 23:39:12 -0300

    fixed travis build

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ cd test-project-5-4 &&
     composer require marabesi/laration:1.0 -vv &&
     php artisan laration:list app &&
     cd .. &&
-    rm -rf test-project-5-4 &&
+    rm -rf test-project-5-4
 
 composer create-project --prefer-dist laravel/laravel test-project-5-5 "5.5.*" -vv
 cd test-project-5-5 &&
     composer require marabesi/laration:1.1 -vv &&
     php artisan laration:list app &&
     cd .. &&
-    rm -rf test-project-5-5 &&
+    rm -rf test-project-5-5
 
 composer create-project --prefer-dist laravel/laravel test-project-5-6 "5.6.*" -vv
 cd test-project-5-6 &&
