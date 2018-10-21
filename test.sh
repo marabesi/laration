aaacc7be3f296d13d32358eb56a78376b920abf6
marabesi <matheus.marabesi@gmail.com>
2018-10-21 13:45:17 +0200

    improved shell script
    in the future would be good to have a for loop instead of repeting the
    code for each laravel version

--- test.sh
+++ test.sh
@@ -1,10 +1,66 @@
 #!/bin/bash
 
-if ./install.sh | grep -q 'app'; then
-    rm -rf test-project
-    echo 'INSTALLATION COMPLETED'
-    exit 0
-else
-    echo 'SOMETHING WENT WRONG'
+composer create-project --prefer-dist -q laravel/laravel test-project-5-4 "5.4.*"
+cd test-project-5-4 &&
+    composer require marabesi/laration:1.0 -vv &&
+    cp ../app.php config/app.php &&
+    php artisan laration:list app &&
+    cd .. &&
+    rm -rf test-project-5-4
+
+if [ -d "test-project-5-4" ]; then
+    echo "ERROR ::: test-project-5-4 folder exists."
     exit 1
 fi
+
+echo "!!!SUCCESS!!!"
+echo "!!!LARAVEL 5.4!!!"
+echo "!!!SUCCESS!!!"
+
+composer create-project --prefer-dist -q laravel/laravel test-project-5-5 "5.5.*"
+cd test-project-5-5 &&
+    composer require marabesi/laration:1.1 -vv &&
+    php artisan laration:list app &&
+    cd .. &&
+    rm -rf test-project-5-5
+
+if [ -d "test-project-5-5" ]; then
+    echo "ERROR ::: test-project-5-5 folder exists."
+    exit 1
+fi
+
+echo "!!!SUCCESS!!!"
+echo "!!!LARAVEL 5.5!!!"
+echo "!!!SUCCESS!!!"
+
+composer create-project --prefer-dist -q laravel/laravel test-project-5-6 "5.6.*"
+cd test-project-5-6 &&
+    composer require marabesi/laration:1.2 -vv &&
+    php artisan laration:list app &&
+    cd .. &&
+    rm -rf test-project-5-6
+
+if [ -d "test-project-5-6" ]; then
+    echo "ERROR ::: test-project-5-6 folder exists."
+    exit 1
+fi
+
+echo "!!!SUCCESS!!!"
+echo "!!!LARAVEL 5.6!!!"
+echo "!!!SUCCESS!!!"
+
+composer create-project --prefer-dist -q laravel/laravel test-project-5-7 "5.7.*"
+cd test-project-5-7 &&
+    composer require marabesi/laration:1.2 -vv &&
+    php artisan laration:list app &&
+    cd .. &&
+    rm -rf test-project-5-7
+
+if [ -d "test-project-5-7" ]; then
+    echo "ERROR ::: test-project-5-7 folder exists."
+    exit 1
+fi
+
+echo "!!!SUCCESS!!!"
+echo "!!!LARAVEL 5.7!!!"
+echo "!!!SUCCESS!!!"
