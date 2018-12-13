881c298421d5efc6f239b9483baf62676f63ef43
marabesi <matheus.marabesi@gmail.com>
2018-12-13 21:18:59 +0100

    more options added to the suite test

--- tests/providers/optionsProvider.php
+++ tests/providers/optionsProvider.php
@@ -79,5 +79,110 @@ return [
             'filesystems'
         ]
     ],
+    'database option' => [
+        'contains' => 'database',
+        'notContains' => [
+            'hashing',
+            'app',
+            'mail',
+            'services',
+            'cache',
+            'session',
+            'queue',
+            'view',
+            'logging',
+            'filesystems'
+        ]
+    ],
+    'cache option' => [
+        'contains' => 'cache',
+        'notContains' => [
+            'hashing',
+            'app',
+            'mail',
+            'services',
+            'database',
+            'session',
+            'queue',
+            'view',
+            'logging',
+            'filesystems'
+        ]
+    ],
+    'session option' => [
+        'contains' => 'session',
+        'notContains' => [
+            'hashing',
+            'app',
+            'mail',
+            'services',
+            'database',
+            'cache',
+            'queue',
+            'view',
+            'logging',
+            'filesystems'
+        ]
+    ],
+    'queue option' => [
+        'contains' => 'queue',
+        'notContains' => [
+            'hashing',
+            'app',
+            'mail',
+            'services',
+            'database',
+            'cache',
+            'session',
+            'view',
+            'logging',
+            'filesystems'
+        ]
+    ],
+    'view option' => [
+        'contains' => 'view',
+        'notContains' => [
+            'hashing',
+            'app',
+            'mail',
+            'services',
+            'database',
+            'cache',
+            'session',
+            'queue',
+            'logging',
+            'filesystems'
+        ]
+    ],
+    'logging option' => [
+        'contains' => 'logging',
+        'notContains' => [
+            'hashing',
+            'app',
+            'mail',
+            'services',
+            'database',
+            'cache',
+            'session',
+            'queue',
+            'view',
+            'filesystems'
+        ]
+    ],
+    'filesystems option' => [
+        'contains' => 'filesystems',
+        'notContains' => [
+            'hashing',
+            'app',
+            'mail',
+            'services',
+            'database',
+            'cache',
+            'session',
+            'queue',
+            'view',
+            'logging',
+        ]
+    ],
 ];
 
